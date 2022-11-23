0983cdc10de6cccc3027b2a57c540d8351e66ec1
Jose Perez <leojjosepf@gmail.com>
2022-11-23 16:11:45 -0600

    Roles de usuario y renderizacion con wildfly

--- src/main/java/org/jperez/webapp/jsf3/services/ProductoServiceImpl.java
+++ src/main/java/org/jperez/webapp/jsf3/services/ProductoServiceImpl.java
@@ -1,5 +1,8 @@
 package org.jperez.webapp.jsf3.services;
 
+import jakarta.annotation.security.DeclareRoles;
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 import org.jperez.webapp.jsf3.entities.Categoria;
@@ -12,6 +15,7 @@ import java.util.Optional;
 
 
 @Stateless
+@DeclareRoles({"USER", "ADMIN"})
 public class ProductoServiceImpl implements ProductoService{
 
     @Inject
@@ -21,35 +25,42 @@ public class ProductoServiceImpl implements ProductoService{
     private CrudRepository<Categoria> repositoryCategoria;
 
     @Override
+    @PermitAll
     public List<Producto> listar() {
         return repository.listar();
     }
 
+    @RolesAllowed({"USER", "ADMIN"})
     @Override
     public Optional<Producto> porId(Long id) {
         return Optional.ofNullable(repository.porId(id));
     }
 
+    @RolesAllowed({"ADMIN"})
     @Override
     public void guardar(Producto producto) {
         repository.guardar(producto);
     }
 
+    @RolesAllowed({"ADMIN"})
     @Override
     public void eliminar(Long id) {
         repository.eliminar(id);
     }
 
+    @RolesAllowed({"USER", "ADMIN"})
     @Override
     public List<Categoria> listarCategorias() {
         return repositoryCategoria.listar();
     }
 
+    @RolesAllowed({"USER", "ADMIN"})
     @Override
     public Optional<Categoria> porIdCategoria(Long id) {
         return Optional.ofNullable(repositoryCategoria.porId(id));
     }
 
+    @RolesAllowed({"USER", "ADMIN"})
     @Override
     public List<Producto> buscarPorNombre(String nombre) {
         return repository.buscarPorNombre(nombre);
